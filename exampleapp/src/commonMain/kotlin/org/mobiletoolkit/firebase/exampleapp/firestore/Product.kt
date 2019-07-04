d758e897bb071383691fa49a8503fd2376f4fa82
Sebastian Owodziń <sebastian.owodzin@gmail.com>
2019-07-04 01:10:38 +0100

    kotlin 1.3.41, trying to fix mapper crash on iOS

--- exampleapp/src/commonMain/kotlin/org/mobiletoolkit/firebase/exampleapp/firestore/Product.kt
+++ exampleapp/src/commonMain/kotlin/org/mobiletoolkit/firebase/exampleapp/firestore/Product.kt
@@ -1,7 +1,6 @@
 package org.mobiletoolkit.firebase.exampleapp.firestore
 
-import kotlinx.serialization.*
-import kotlinx.serialization.internal.SerialClassDescImpl
+import kotlinx.serialization.Serializable
 import org.mobiletoolkit.firebase.firestore.FirestoreModel
 
 /**
@@ -19,76 +18,39 @@ data class Product(
 //        override val descriptor: SerialDescriptor = object : SerialClassDescImpl("Product") {
 //            init {
 //                addElement("name")
-//                addElement("dek")
+//                addElement("description")
 //                addElement("price")
 //            }
 //        }
 //
-//        override fun serialize(output: Encoder, obj: Product) {
-//            with(output.beginStructure(descriptor)) {
-//                encodeStringElement(descriptor, 0, obj.name)
-//                encodeStringElement(descriptor, 1, obj.dek)
-//                encodeDoubleElement(descriptor, 2, obj.price)
-//
-//                endStructure(descriptor)
-//            }
-//        }
-//
-//        override fun deserialize(decoder: Decoder): Product {
-//            lateinit var name: String
-//            lateinit var dek: String
-//            var price = 0.0
-//
-//            with(decoder.beginStructure(descriptor)) {
-//                loop@ while (true) {
-//                    val i = decodeElementIndex(descriptor)
-//
-//                    println("i: $i")
-//
-//                    when (i) {
-////                    when (val i = decodeElementIndex(descriptor)) {
-//                        CompositeDecoder.READ_DONE -> break@loop
-//                        CompositeDecoder.READ_ALL -> break@loop
-//                        0 -> name = decodeStringElement(descriptor, i)
-//                        1 -> dek = decodeStringElement(descriptor, i)
-//                        2 -> price = decodeDoubleElement(descriptor, i)
-//                        else -> throw SerializationException("Unknown index $i")
-//                    }
-//                }
-//
-//                endStructure(descriptor)
-//            }
-//
-//            return Product(name, dek, price)
-//        }
-//    }
-
-//    @Serializer(forClass = Product::class)
-//    companion object : KSerializer<Product> {
-//        override val descriptor = StringDescriptor.withName("Product")
-//
 //        override fun serialize(encoder: Encoder, obj: Product) {
-//            encoder.encodeString(obj.name)
-//            encoder.encodeString(obj.description)
-//            encoder.encodeDouble(obj.price)
+//            val compositeOutput = encoder.beginStructure(descriptor)
+//            compositeOutput.encodeStringElement(descriptor, 0, obj.name)
+//            compositeOutput.encodeStringElement(descriptor, 1, obj.description)
+//            compositeOutput.encodeDoubleElement(descriptor, 2, obj.price)
+//            compositeOutput.endStructure(descriptor)
 //        }
 //
 //        override fun deserialize(decoder: Decoder): Product {
-//            val price = decoder.decodeDouble()
-//            val name = decoder.decodeString()
-//            val description = decoder.decodeString()
-//
+//            val dec: CompositeDecoder = decoder.beginStructure(descriptor)
+//            var name: String? = null
+//            var description: String? = null
+//            var price: Double? = null
+//            loop@ while (true) {
+//                when (val i = dec.decodeElementIndex(descriptor)) {
+//                    CompositeDecoder.READ_DONE -> break@loop
+//                    0 -> name = dec.decodeStringElement(descriptor, i)
+//                    1 -> description = dec.decodeStringElement(descriptor, i)
+//                    2 -> price = dec.decodeDoubleElement(descriptor, i)
+//                    else -> throw SerializationException("Unknown index $i")
+//                }
+//            }
+//            dec.endStructure(descriptor)
 //            return Product(
-//                name,
-//                description,
-//                price
+//                name ?: throw MissingFieldException("name"),
+//                description ?: throw MissingFieldException("description"),
+//                price ?: throw MissingFieldException("price")
 //            )
-//
-////            return Product(
-////                decoder.decodeString(),
-////                decoder.decodeString(),
-////                decoder.decodeDouble()
-////            )
 //        }
 //    }
 }
--- exampleapp/src/iosMain/kotlin/org/mobiletoolkit/firebase/exampleapp/firestore/ProductsRepository.kt
+++ exampleapp/src/iosMain/kotlin/org/mobiletoolkit/firebase/exampleapp/firestore/ProductsRepository.kt
@@ -1,14 +1,9 @@
 package org.mobiletoolkit.firebase.exampleapp.firestore
 
 import com.google.firebase.firestore.FIRDocumentSnapshot
-import com.google.firebase.firestore.FIRFirestore
-import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.Mapper
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.stringify
 import org.mobiletoolkit.firebase.firestore.Firestore
 import org.mobiletoolkit.firebase.firestore.FirestoreRepository
-import platform.Foundation.NSThread
 
 /**
  * Created by Sebastian Owodzin on 18/05/2019.
@@ -18,17 +13,16 @@ actual class ProductsRepository : FirestoreRepository<Product>(Firestore.firesto
     override val collectionPath = "/products"
 
     override fun deserialize(snapshot: FIRDocumentSnapshot): Product? =
-//        snapshot.data()?.let { Mapper.unmap(Product.serializer(), it as Map<String, Any>) } // FIXME - this seems to be not working on iOS
         snapshot.data()?.let {
             try {
-                println("data: $it")
-
                 val data = it as Map<String, Any>
 
                 Product(data["name"] as String, data["description"] as String, data["price"] as Double)
+//                Mapper.unmap(Product.serializer(), it as Map<String, Any>) // FIXME - this seems to be not working on iOS
             } catch (e: Exception) {
                 e.printStackTrace()
-                println("exception: ${e.cause?.message}")
+                println("exception: ${e.message}")
+//                println("exception: ${e.cause?.message}")
                 null
             }
         }
